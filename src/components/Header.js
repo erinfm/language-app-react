2d02574f5708b724b7c5952d9f6e941fe977a311
Erin M <erinfranmc@gmail.com>
2019-05-16 17:15:54 +0100

    added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+function Header() {
+  const NavBar = styled.div`
+    padding: 0 2rem;
+    display: flex;
+    align-items: center;
+    background: #fff;
+    border-bottom: 3px solid #ede7f6;
+    height: 4rem;
+    width: 100%;
+  `;
+
+  const Title = styled.h1`
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: #6200ea;
+    cursor: pointer;
+  `;
+
+  return (
+    <>
+      <NavBar>
+        <Title>Header</Title>
+      </NavBar>
+    </>
+  );
+}
+
+export default Header;
