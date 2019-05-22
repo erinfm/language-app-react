7bef31e5bf655b6d53cde157ec0f9b803bf9f57b
Erin M <erinfranmc@gmail.com>
2019-05-22 16:55:36 +0100

    Adding quiz functionality

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,19 +3,30 @@ import styled from "styled-components/macro";
 
 function Quiz() {
   const QuizContainer = styled.div`
-    padding: 2rem 2rem;
-    background: #8559da;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 2rem 10rem;
+    background: #f5f5f5;
     height: calc(100vh - 3.5rem);
 
     @media (max-width: 700px) {
-      height: 100vh;
+      padding: 2rem 2rem;
     }
   `;
 
   const Title = styled.h1`
+    top: 20vh;
     font-size: 2rem;
-    font-weight: 400;
-    color: #fff;
+    font-weight: 600;
+    color: #555;
+    margin-bottom: 2rem;
+  `;
+
+  const Subtitle = styled(Title)`
+    color: #444;
+    font-size: 1.25rem;
+    line-height: 2rem;
   `;
 
   const Button = styled.button`
@@ -26,14 +37,26 @@ function Quiz() {
     cursor: pointer;
     font-size: 1.25rem;
     padding: 1rem 1.75rem;
-    position: absolute;
-    bottom: 2rem;
-    left: 2rem;
     transition: background 0.5s ease;
+    max-width: 200px;
 
     &:hover {
       background: #5c1cea;
     }
+
+    // // Margin keeps elements vertically centered on mobile devices
+    // @media (max-width: 700px) {
+    //   margin-bottom: 3.5rem;
+    // }
+  `;
+
+  const TopicList = styled.div`
+    color: #444;
+    font-size: 1.25rem;
+    line-height: 2rem;
+    list-style-position: inside;
+    padding: 2rem 0;
+    margin-bottom: 2rem;
   `;
 
   const Question = styled.span`
@@ -53,27 +76,76 @@ function Quiz() {
     left: 50%;
     top: 50%;
     display: flex;
-    flex-direction: column;
   `;
   const Option = styled(Question)`
     position: static;
     background: lightblue;
   `;
 
-  const [quizState, setQuizState] = useState("initial");
+  const VocabBlock = styled.div`
+    display: flex;
+    font-size: 2rem;
+
+    @media (max-width: 700px) {
+      flex-direction: column;
+      font-size: 1.5rem;
+    }
+  `;
+
+  const Vocab = styled.p`
+    padding-right: 10rem;
+
+    @media (max-width: 700px) {
+      padding: 1.5rem 0;
+    }
+  `;
+
+  // MarginBlock component keeps elements correcly vertically centered on mobile devices
+  const MarginBlock = styled.div`
+    @media (max-width: 700px) {
+      margin-bottom: 3.5rem;
+    }
+  `;
+
+  const [quizState, setQuizState] = useState("round1");
 
   if (quizState === "initial") {
     return (
       <>
         <QuizContainer>
           <Title>Let's learn!</Title>
+          <TopicList>
+            In Level 1 you will learn:
+            <ul>
+              <li>Colours</li>
+              <li>Animals</li>
+            </ul>
+          </TopicList>
           <Button onClick={() => setQuizState("round1")}>Start Quiz</Button>
+          <MarginBlock />
         </QuizContainer>
       </>
     );
   }
 
   if (quizState === "round1") {
+    return (
+      <>
+        <QuizContainer>
+          <Title>Basic Colours</Title>
+          <Subtitle>Here are some colours!</Subtitle>
+          <VocabBlock>
+            <Vocab>Rojo</Vocab>
+            <Vocab>Azul</Vocab>
+            <Vocab>Verde</Vocab>
+          </VocabBlock>
+          <MarginBlock />
+        </QuizContainer>
+      </>
+    );
+  }
+
+  if (quizState === "round3") {
     return (
       <>
         <QuizContainer>
@@ -84,6 +156,7 @@ function Quiz() {
             <Option>Answer</Option>
             <Option>Answer</Option>
           </OptionBlock>
+          <MarginBlock />
         </QuizContainer>
       </>
     );
